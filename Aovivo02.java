fe61ce68e55ffdee04cbdbee2246e43a4aa15e21
Fabiano Taveski <nanolc754@gmail.com>
2024-09-09 17:08:42 -0300

    Add files via upload

---
+++ Aovivo02.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aovivo02;
+
+import aovivo02.telas.TelaLogin;
+import aovivo02.telas.Telainicial;
+
+
+public class Aovivo02 {
+
+   
+    public static void main(String[] args) {
+      
+        
+    // atalho para importar classe ctrl+shift+i
+   
+    
+    TelaLogin tl = new TelaLogin(); {// instanciando tela inicial
+    tl.setVisible(true);
+}
+
+}
+}
+
